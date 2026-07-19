3d33877a3a388182e04e756b6c087d1e297fdd06
YOGA <yogaprasath16@gmail.com>
2026-07-19 11:24:49 +0000

    Inital commit

--- appointment.py
+++ appointment.py
@@ -1,1 +1,6 @@
 print("Hospital Appointment Booking System")
+#commit
+def register_patient(name,age):
+    print(f"Patient {name},Age {age} registered successfully.")
+    
+register_patient("Albert",39)
